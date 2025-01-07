9177c48603215d60a9479fe7a881da8f653731ad
Heim_Sendir <alex.noa29@gamil.com>
2025-01-07 13:33:52 +0500

    add api

--- cats/views.py
+++ cats/views.py
@@ -7,9 +7,9 @@ from .serializers import CatSerializer
 
 
 @api_view(['GET', 'POST'])
-def cat_list(request):
+def cat_add_or_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -17,3 +17,30 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_edit_or_delete(request, pk):
+    if request.method == 'PATCH':
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        cat = Cat.objects.get(pk=pk)
+        cat.delete()
+        return Response({"message": "Запись успешно удалена."},
+                        status=status.HTTP_204_NO_CONTENT)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import cat_add_or_list, cat_edit_or_delete
 
 urlpatterns = [
-   path('cats/', cat_list),
+   path('cats/', cat_add_or_list),
+   path('cats/<int:pk>/', cat_edit_or_delete),
 ]
-
-
